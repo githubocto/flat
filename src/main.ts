5554f1304240748ceeafa162b8bf248831b9b71b
Matt Rothenberg <mattrothenberg@users.noreply.github.com>
2021-03-01 14:39:44 -0500

    fix: typo

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function run (): Promise<void> {
   core.startGroup('Calculating diffstat')
   await exec('git', ['add', '-A'])
   const bytes = await diff()
-  core.setOutput('deltabytes', bytes)
+  core.setOutput('delta_bytes', bytes)
   core.endGroup()
 
   if (bytes === 0) {
